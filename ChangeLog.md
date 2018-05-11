f39edafde01a5685372385d5d6bb1f7ab7c9f74c
Benjamin Altieri <benaltieri@gmail.com>
2018-05-11 10:19:58 -0700

    Fiyah

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for killer-slackbot
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib
+    ( someFunc
+    ) where
+
+import System.IO
+import System.Environment
+import qualified Configuration.Dotenv as Dotenv
+import Configuration.Dotenv.Types (defaultConfig)
+import Network.Wreq
+import qualified Data.Text as T
+import Control.Lens ((&), (^.), (.~))
+import qualified Data.ByteString.Lazy.Char8 as BSL
+
+someFunc :: IO ()
+someFunc = do
+    -- don't buffer IO
+    hSetBuffering stdout NoBuffering
+    -- load our .env and get our slackbot token
+    Dotenv.loadFile defaultConfig
+    token <- T.pack <$> getEnv "SLACK_API_TOKEN"
+    -- setup our API request
+    let opts = defaults & param "token" .~ [token]
+    r <- getWith opts "https://slack.com/api/rtm.connect"
+    putStrLn $ BSL.unpack $ r ^. responseBody
