4b535bda860a7f961eb1bfbc5e829b60b96335cf
Benjamin Altieri <benaltieri@gmail.com>
2018-05-16 16:16:01 -0700

    calibrating.. self awareness achieved

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,8 +10,14 @@ import qualified Configuration.Dotenv as Dotenv
 import Configuration.Dotenv.Types (defaultConfig)
 import Network.Wreq
 import qualified Data.Text as T
-import Control.Lens ((&), (^.), (.~))
+import Control.Lens ((&), (^?), (.~))
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Aeson.Lens
+import Wuss hiding (defaultConfig)
+import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData)
+import Network.URI
+import Control.Monad (forever)
+import Data.Aeson
 
 resume_prime_directive :: IO ()
 resume_prime_directive = do
@@ -23,4 +29,33 @@ resume_prime_directive = do
     -- setup our API request
     let opts = defaults & param "token" .~ [token]
     r <- getWith opts "https://slack.com/api/rtm.connect"
-    putStrLn $ BSL.unpack $ r ^. responseBody
+    let murl = T.unpack <$> r ^? responseBody . key "url" . _String
+        muri = parseURI =<< murl
+        mhost = uriRegName <$> (uriAuthority =<< muri)
+        mpath = uriPath <$> muri
+
+    case (mhost, mpath) of
+        (Just host, Just path) -> runSecureClient host 443 path ws
+        otherwise -> fail "Could not obtain Slack RTM URL"
+
+ws :: ClientApp ()
+ws connection = do
+    print "Connected!"
+    forever $ do
+        message <- receiveData connection
+        case decode message of
+          Nothing -> pure ()
+          Just msg -> print $ content msg
+
+data SlackMessage = SlackMessage
+    { userId :: String
+    , content :: String
+    , channelId :: String
+    } deriving (Show)
+
+instance FromJSON SlackMessage where
+    parseJSON = withObject "SlackMessage" $ \o -> SlackMessage
+        <$> o .: "user"
+        <*> o .: "text"
+        <*> o .: "channel"
+
